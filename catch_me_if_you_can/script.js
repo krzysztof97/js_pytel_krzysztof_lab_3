c195e88064929b1457414d4e4a147042817df6c1
Krzysztof Pytel <37655632+krzysztof97@users.noreply.github.com>
2019-02-19 22:54:47 +0100

    cmiyc: Show other users on map, Add players icons

--- catch_me_if_you_can/script.js
+++ catch_me_if_you_can/script.js
@@ -1,7 +1,14 @@
+let socketKey = "fj3Dk83A9fGr4Dc2"; // filtrowanie wiadomości z websocket po tym kluczu (nagłówek wiadomości)
 let uluru, map, marker
-let ws
+let ws;
 let players = {}
 let nick = '1'
+let begForLocation = document.querySelector('.beg-for-location')
+let guid = parseInt(Date.now() + Math.random() * 1000) // unikatowy identyfikator
+let icon = guid % 5 // numer obrazka
+
+//ws = new WebSocket("ws://91.121.6.192:8010")
+ws = new WebSocket("ws://77.55.222.58:8010")
 
 function initMap()
 {
@@ -11,7 +18,7 @@ function initMap()
         center: uluru,
         keyboardShortcuts: false
     });
-    marker = new google.maps.Marker({position: uluru, map: map});
+    marker = new google.maps.Marker({position: uluru, map: map, icon: iconString(icon)});
 
 }
 
@@ -22,25 +29,23 @@ function getLocalization()
 
 function geoOk(data)
 {
+    begForLocation.classList.remove('beg-for-location--visible');
     let coords = {
         lat: data.coords.latitude,
         lng: data.coords.longitude
     };
-    map.setCenter(coords)
-    marker.setPosition(coords)
+    placeMyMarker(coords, 'new')
 
 }
 
 function geoFail(data)
 {
-    console.log(data);
+    begForLocation.classList.add('beg-for-location--visible');
 }
-
-getLocalization()
 
 function watchKeys()
 {
-    document.addEventListener('keypress', moveMarker)
+    document.addEventListener('keydown', moveMarker)
 }
 
 function moveMarker(e)
@@ -67,8 +72,64 @@ function moveMarker(e)
         default:
             break;
     }
-
-    marker.setPosition(coords)
+    placeMyMarker(coords, 'move')
 }
 
+function placeMyMarker(_coords, _action)
+{
+    marker.setPosition(_coords)
+    map.setCenter(_coords)
+
+    let me = {
+        id: guid,
+        action: _action,
+        coords: _coords,
+        playericon: icon
+    }
+
+    sendMessage(me)
+}
+
+function sendMessage(obj)
+{
+    ws.send(socketKey + JSON.stringify(obj))
+}
+
+function receiveMessage(msg)
+{
+    if(msg.substring(0, socketKey.length) == socketKey)
+    {
+        msg = msg.substring(socketKey.length)
+        msg = JSON.parse(msg)
+        console.log(msg)
+        if(msg.id != guid)
+        {
+            if(msg.action == 'new')
+            {
+                players[msg.id] = new google.maps.Marker({position: msg.coords, map: map, icon: iconString(msg.playericon)});
+            }
+            if(msg.action == 'move')
+            {
+                if(players[msg.id])
+                    players[msg.id].setPosition(msg.coords);
+                else
+                    players[msg.id] = new google.maps.Marker({position: msg.coords, map: map, icon: iconString(msg.playericon)});
+            }
+        }
+    }
+}
+
+getLocalization()
+
 watchKeys()
+
+ws.onmessage = function(e) 
+{ 
+    let msg = e.data
+    receiveMessage(msg)
+};
+
+function iconString(number)
+{
+    return `icon/${number}.png`
+}
