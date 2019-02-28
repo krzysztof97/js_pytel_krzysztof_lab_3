6a5ac503e706d76be146b7b764acd039f9c85bb3
Krzysztof Pytel <37655632+krzysztof97@users.noreply.github.com>
2019-02-28 21:51:30 +0100

    ball: Add changing ball position and hide

--- ball_in_a_hole/script.js
+++ ball_in_a_hole/script.js
@@ -1,30 +1,60 @@
-let ball, level, ballSpeedX, ballSpeedY, ballPosX, ballPosY;
+let ball, level, ballSpeedX, ballSpeedY, ballPosX, ballPosY, ballSizeX, ballSizeY, playgroundX, playgroundY, holePosX, holePosY, holeSizeX, holeSizeY
 
 function initialize() {
-    window.addEventListener('deviceorientation', orientationChange);
-    ball = document.querySelector('.ball');
-    level = document.querySelector('.level');
-    ballSpeedX = 0;
-    ballSpeedY = 0;
-    ballPosX = ball.style.left;
-    ballPosY = ball.style.bottom;
+    window.addEventListener('deviceorientation', orientationChange)
+    ball = document.querySelector('.ball')
+    level = document.querySelector('.level')
+    ballSpeedX = 0
+    ballSpeedY = 0
+    ballPosX = 0
+    ballPosY = 00
+    ballSizeX = 30
+    ballSizeY = 30
+    playgroundX = 300
+    playgroundY = 460
+    holePosX = 130
+    holePosY = 290
+    holeSizeX = 40
+    holeSizeY = 40
 }
 
-initialize();
-
-console.log(ballPosX);
+initialize()
 
 function orientationChange(event) {
     //event.alpha 0 360, event.beta -180 180, event.gamma -90 90
-    ballSpeedX = -1*event.beta
-    ballSpeedY = 1*event.gamma
+    ballSpeedX = 1*event.gamma
+    ballSpeedY = -1*event.beta
 }
 
 function moveBall() 
 {
-    console.log(ballSpeedX);
-    console.log(ballSpeedY);
-    setTimeout(moveBall, 1000/60);
+    ballPosX = Math.min( Math.max( 0, ballPosX+ballSpeedX ), playgroundX-ballSizeX )
+    ballPosY = Math.min( Math.max( 0, ballPosY+ballSpeedY ), playgroundY-ballSizeY )
+
+    setBallPos()
+
+    if(ballInTheHole())
+    {
+
+    }
+    else
+    {
+        setTimeout(moveBall, 1000/60)
+    }
 }
 
-moveBall();
+moveBall()
+
+function setBallPos()
+{
+    ball.style.left = ballPosX + "px"
+    ball.style.bottom = ballPosY + "px"
+}
+
+function ballInTheHole()
+{
+    if(ballPosX >= holePosX && ballPosX+ballSizeX <= holePosX+holeSizeX && ballPosY >= holePosY && ballPosY+ballSizeY <= holePosY+holeSizeY)
+    {
+        ball.remove()
+    }
+}
