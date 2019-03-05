5bc08a0c9b4caa649985539cfb9006a768f4c21b
Krzysztof Pytel <37655632+krzysztof97@users.noreply.github.com>
2019-03-05 22:34:05 +0100

    Catch: Add chat feature

--- catch_me_if_you_can/script.js
+++ catch_me_if_you_can/script.js
@@ -7,6 +7,9 @@ let begForLocation = document.querySelector('.beg-for-location')
 let guid = parseInt(Date.now() + Math.random() * 1000) // unikatowy identyfikator
 let icon = guid % 5 // numer obrazka
 
+//elementy chatu
+let chatNick, chatContent, chatMessages, chatForm
+
 //ws = new WebSocket("ws://91.121.6.192:8010")
 ws = new WebSocket("ws://77.55.222.58:443")
 
@@ -20,6 +23,20 @@ function initMap()
     });
     marker = new google.maps.Marker({position: uluru, map: map, icon: iconString(icon)});
 
+    initChat()
+}
+
+// funckja inicjujca elementy chatu
+function initChat()
+{
+    chatNick = document.querySelector('#nick')
+    chatContent = document.querySelector('#content')
+    chatMessages = document.querySelector('#messages')
+    chatForm = document.querySelector('#form')
+    chatForm.addEventListener('submit', function(e){
+        e.preventDefault()
+        sendChatMessage()
+    })
 }
 
 function getLocalization()
@@ -101,7 +118,7 @@ function receiveMessage(msg)
     {
         msg = msg.substring(socketKey.length)
         msg = JSON.parse(msg)
-        console.log(msg)
+        
         if(msg.id != guid)
         {
             if(msg.action == 'new')
@@ -118,6 +135,10 @@ function receiveMessage(msg)
             if(msg.action == 'close')
             {
                 players[msg.id].setMap(null)
+            }
+            if(msg.action == 'chat')
+            {
+                showMessage(msg)
             }
         }
     }
@@ -144,4 +165,34 @@ window.onbeforeunload = function(e)
 function iconString(number)
 {
     return `icon/${number}.png`
+}
+
+function sendChatMessage()
+{
+    let msg = {
+        id: -1,
+        nick: chatNick.value,
+        content: chatContent.value,
+        time: new Date(),
+        action: 'chat'
+    }
+
+    if(msg.nick.length > 0 && msg.content.length > 0)
+    {
+        chatContent.value = ''
+        sendMessage(msg)
+    }
+}
+
+function showMessage(msg)
+{
+    let messageDOMObject = 
+    `<div class="message">
+        <div class="message__header">
+            <div class="message__nick">${msg.nick}</div>
+            <div class="message__time">(${msg.time})</div>
+        </div>
+        <div class="message__content">${msg.content}</div>
+    </div>`
+    chatMessages.innerHTML = messageDOMObject + chatMessages.innerHTML
 }
