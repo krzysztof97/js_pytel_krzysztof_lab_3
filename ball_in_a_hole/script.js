ec74291c8045d6d4343b18031b266f5a0e71cd8c
Krzysztof Pytel <37655632+krzysztof97@users.noreply.github.com>
2019-03-05 20:59:45 +0100

    Ball: Add time counter

--- ball_in_a_hole/script.js
+++ ball_in_a_hole/script.js
@@ -1,13 +1,19 @@
-let ball, level, ballSpeedX, ballSpeedY, ballPosX, ballPosY, ballSizeX, ballSizeY, playgroundX, playgroundY, holePosX, holePosY, holeSizeX, holeSizeY
+let ball, level, menu
+let ballSpeedX, ballSpeedY, ballPosX, ballPosY, ballSizeX, ballSizeY
+let playgroundX, playgroundY
+let holePosX, holePosY, holeSizeX, holeSizeY
+let start, lastTime
+let gameRunning = false
+let startTime, endTime
 
 function initialize() {
-    window.addEventListener('deviceorientation', orientationChange)
     ball = document.querySelector('.ball')
     level = document.querySelector('.level')
-    ballSpeedX = 0
-    ballSpeedY = 0
-    ballPosX = 0
-    ballPosY = 00
+    menu = document.querySelector('.menu')
+    lastTime = document.querySelector('#lastTime')
+    window.addEventListener('deviceorientation', orientationChange)
+    start = document.querySelector('#start')
+    start.addEventListener("click", startGame)
     ballSizeX = 30
     ballSizeY = 30
     playgroundX = 300
@@ -18,7 +24,22 @@ function initialize() {
     holeSizeY = 40
 }
 
-initialize()
+document.addEventListener("DOMContentLoaded", initialize)
+
+function startGame()
+{
+    setBallPos()
+    level.classList.remove("level--hidden");
+    menu.classList.add("menu--hidden");
+    ballSpeedX = 0
+    ballSpeedY = 0
+    ballPosX = 0
+    ballPosY = 0
+    gameRunning = true
+    moveBall()
+    startTime = Date.now()
+}
+
 
 function orientationChange(event) {
     //event.alpha 0 360, event.beta -180 180, event.gamma -90 90
@@ -28,6 +49,10 @@ function orientationChange(event) {
 
 function moveBall() 
 {
+    if(!gameRunning)
+    {
+        return;
+    }
     ballPosX = Math.min( Math.max( 0, ballPosX+ballSpeedX ), playgroundX-ballSizeX )
     ballPosY = Math.min( Math.max( 0, ballPosY+ballSpeedY ), playgroundY-ballSizeY )
 
@@ -35,15 +60,13 @@ function moveBall()
 
     if(ballInTheHole())
     {
-
+        endGame()
     }
     else
     {
         setTimeout(moveBall, 1000/60)
     }
 }
-
-moveBall()
 
 function setBallPos()
 {
@@ -55,6 +78,25 @@ function ballInTheHole()
 {
     if(ballPosX >= holePosX && ballPosX+ballSizeX <= holePosX+holeSizeX && ballPosY >= holePosY && ballPosY+ballSizeY <= holePosY+holeSizeY)
     {
-        ball.remove()
+        return true
     }
+    return false
+}
+
+function endGame()
+{
+    gameRunning = false
+    level.classList.add("level--hidden");
+    menu.classList.remove("menu--hidden");
+    endTime = Date.now()
+    updateTime()
+}
+
+function updateTime()
+{
+    let fullMiliseconds = endTime - startTime
+    let seconds = Math.floor(fullMiliseconds/1000)
+    let miliseconds = fullMiliseconds%1000
+
+    lastTime.innerHTML = `${seconds} seconds ${miliseconds} miliseconds`
 }
